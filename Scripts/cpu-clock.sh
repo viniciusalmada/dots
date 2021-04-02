1624e24650447a1511e8c8820ace8a64128b6c5b
Vinicius Almada <viniciusalmada10@gmail.com>
2021-04-01 21:37:31 -0300

    Update and rename pcpu-clock.sh to cpu-clock.sh

--- Scripts/cpu-clock.sh
+++ Scripts/cpu-clock.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 CL=`lscpu | grep "CPU MHz" | awk {'print $3'}`
-TMP=`sensors | grep "Core 0" | awk {'print substr($3,2)'} `
+#TMP=`sensors | grep "Core 0" | awk {'print substr($3,2)'} `
 
 OUT=`echo $CL | awk '{ printf "%4.0f MHz", $1  }'`
 
 echo "&#xf2db; <span color='#fff'>$OUT</span>"
-
-#echo "$OUT"
-#echo "$OUT"
-#echo "#FFFFFF"
-#echo "#282828"
