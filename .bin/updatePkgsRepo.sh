4050bcddbb6d4a58714ec3a03a174e0cc57a3820
Vinicius Almada <viniciusalmada10@gmail.com>
2021-04-01 21:27:36 -0300

    Delete .bin directory

--- .bin/updatePkgsRepo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-yay -Qq > ~/.installed-pkgs && yay -Q > ~/.installed-pkgs-versions
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME add  ~/.installed-pkgs  ~/.installed-pkgs-versions
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME commit -m "$1"
