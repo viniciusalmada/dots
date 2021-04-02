2634d51a43691dc24fe9af2276b3bd5078294a4f
Vinicius Almada <viniciusalmada10@gmail.com>
2021-04-01 21:51:26 -0300

    Update and rename pdisk.sh to free-disk.sh

--- Scripts/free-disk.sh
+++ Scripts/free-disk.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-USED=`df /dev/sda4 | grep sda4 | awk '{ print $3 }'`
-PERC=`df -h /dev/sda4 | grep sda4 | awk '{ print $5 }'`
+USED=`df -h . | grep dev | awk '{ print $3 }'`
+PERC=`df -h . | grep dev | awk '{ print $5 }'`
 
 USED_G=`perl -E "say $USED/1024.0/1024.0"`
 
 OUT=`echo $USED_G $PERC | awk '{ printf "%1.2f GB %s", $1, $2 }'`
 
 echo "&#xf7c2; <span color='#fff'>$OUT</span>"
-
